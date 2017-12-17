895253a08ac1b84faf4e682bdb8433aa1a598f8d
maciekmichalik <michalikmaciej1995@gmail.com>
2017-12-17 18:02:42 +0100

    Fix controller

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::InvitationsController < ApplicationController
   before_action :verify_current_user_to_be_employee
   before_action :set_driving_school
+  before_action :set_invited_user_type
 
   def create
-    authorize @driving_school, :can_manage_employees? if invited_user_type == 'Employee'
-    authorize @driving_school, :can_manage_students? if invited_user_type == 'Student'
+    authorize @driving_school, :can_manage_employees? if @invited_user_type == 'Employee'
+    authorize @driving_school, :can_manage_students? if @invited_user_type == 'Student'
 
     CreateInvitationService.new(
       @driving_school,
-      invited_user_type,
+      @invited_user_type,
       invited_user_params,
       invited_user_privileges_params
     ).call
@@ -22,13 +23,15 @@ class Api::V1::InvitationsController < ApplicationController
     params.require(:user).permit(:email, :name, :surname)
   end
 
-  def invited_user_type
-    params.require(:user).permit(:type)[:type]
+  def set_invited_user_type
+    @invited_user_type = params.require(:user).permit(:type)[:type]
   end
 
   def invited_user_privileges_params
-    params.require(:employee_privilege_set).permit(:can_manage_employees, :can_manage_students, :can_modify_schedules,
-                                            :is_driving)
+    if @invited_user_type == 'Employee'
+      params.require(:employee_privilege_set).permit(:can_manage_employees, :can_manage_students, :can_modify_schedules,
+                                                     :is_driving)
+    end
   end
 
   def set_driving_school
