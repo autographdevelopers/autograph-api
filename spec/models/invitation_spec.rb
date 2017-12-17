8eeca9210a18912491f04c8c5723d2672c241c9a
maciekmichalik <michalikmaciej1995@gmail.com>
2017-12-17 17:57:41 +0100

    Add tests

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,5 +1,9 @@
-require 'rails_helper'
+describe Invitation do
+  context 'relations' do
+    it { should belong_to(:invitable) }
+  end
 
-RSpec.describe Invitation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { should validate_presence_of(:email) }
+  end
 end
