6ac1d44d3180c0f037699d7b9d02af58f62ccba0
maciekmichalik <michalikmaciej1995@gmail.com>
2018-02-18 21:55:25 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+employee = FactoryBot.create(:employee, email: 'employee@gmail.com', password: 'password')
+student = FactoryBot.create(:student, email: 'student@gmail.com', password: 'password')
+owner = FactoryBot.create(:employee, email: 'owner@gmail.com', password: 'password')
+
+DrivingSchool.statuses.keys.each do |status|
+  EmployeeDrivingSchool.statuses.keys.each do |e_status|
+    FactoryBot.create(:employee_driving_school, driving_school: FactoryBot.create(:driving_school, status: status), employee: employee, status: e_status)
+  end
+
+  EmployeeDrivingSchool.statuses.keys.each do |o_status|
+    FactoryBot.create(:employee_driving_school, driving_school: FactoryBot.create(:driving_school, status: status), employee: owner, status: o_status, is_owner: true)
+  end
+
+  StudentDrivingSchool.statuses.keys.each do |s_status|
+    FactoryBot.create(:student_driving_school, driving_school: FactoryBot.create(:driving_school, status: status), student: student, status: s_status)
+  end
+end
+
+DrivingSchool.where(status: :active).each do |driving_school|
+  5.times do |i|
+    eds = FactoryBot.create(:employee_driving_school, driving_school: driving_school, status: :active)
+    eds.employee_privilege_set.update(
+      can_manage_employees: i == 0,
+      can_manage_students: i == 1,
+      can_modify_schedules: i == 2,
+      is_driving: i == 3,
+      is_owner: i == 4
+    )
+
+    FactoryBot.create(:student_driving_school, driving_school: driving_school, status: :active)
+  end
+
+  FactoryBot.create(:employee_driving_school, driving_school: driving_school, status: :archived)
+  FactoryBot.create(:student_driving_school, driving_school: driving_school, status: :archived)
+
+  FactoryBot.create(:employee_driving_school, driving_school: driving_school, status: :pending)
+  FactoryBot.create(:student_driving_school, driving_school: driving_school, status: :pending)
+end
