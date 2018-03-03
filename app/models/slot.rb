ee643da74db9f636826eddb16e21373777072c8c
maciekmichalik <michalikmaciej1995@gmail.com>
2018-03-03 23:33:57 +0100

    Add scopes to Slot model

--- app/models/slot.rb
+++ app/models/slot.rb
@@ -5,4 +5,9 @@ class Slot < ApplicationRecord
 
   # == Validations ============================================================
   validates :start_time, presence: true
+
+  # == Scopes =================================================================
+  scope :booked,    -> { where.not(driving_lesson: nil) }
+  scope :available, -> { where(driving_lesson: nil) }
+  scope :future,    -> { where('start_time > ?', Time.now) }
 end
