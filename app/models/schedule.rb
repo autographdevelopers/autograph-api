2b43cdd850b68541e8706cb1aef5fd2f350ed3c9
maciekmichalik <michalikmaciej1995@gmail.com>
2018-03-03 23:39:17 +0100

    Refactor validations on Schedule model

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -20,20 +20,37 @@ class Schedule < ApplicationRecord
   # == Validations ============================================================
   validates :current_template, :new_template, :new_template_binding_from, :repetition_period_in_weeks, presence: true
   validates :repetition_period_in_weeks, inclusion: MIN_SCHEDULE_REPETITION_PERIOD..MAX_SCHEDULE_REPETITION_PERIOD
-  validate :template_format, if: -> { template.present? }
+  validate :current_template_format
+  validate :new_template_format
+  validate :new_template_binding_from_to_be_in_the_future
 
-  def template_format
-    errors.add(:template, "has invalid weekday(s)") unless template_contains_valid_weekdays?
-    errors.add(:template, "has invalid slot_start_times_id(s)") unless template_contains_valid_slot_start_times_ids?
+  def new_template_binding_from_to_be_in_the_future
+    if new_template_binding_from.present? &&  new_template_binding_from_changed? && new_template_binding_from <= Date.today
+      errors.add(:new_template_binding_from, "must be in the future")
+    end
+  end
+
+  def current_template_format
+    if current_template.present?
+      errors.add(:current_template, "has invalid weekday(s)") unless template_contains_valid_weekdays?(current_template)
+      errors.add(:current_template, "has invalid slot_start_times_id(s)") unless template_contains_valid_slot_start_times_ids?(current_template)
+    end
+  end
+
+  def new_template_format
+    if new_template.present?
+      errors.add(:new_template, "has invalid weekday(s)") unless template_contains_valid_weekdays?(new_template)
+      errors.add(:new_template, "has invalid slot_start_times_id(s)") unless template_contains_valid_slot_start_times_ids?(new_template)
+    end
   end
 
   private
 
-  def template_contains_valid_weekdays?
+  def template_contains_valid_weekdays?(template)
     WEEKDAYS.sort == template.keys.sort
   end
 
-  def template_contains_valid_slot_start_times_ids?
+  def template_contains_valid_slot_start_times_ids?(template)
     template.each do |_, slot_start_times_ids|
       return false if (slot_start_times_ids - SLOT_START_TIMES.keys).any?
       return false if slot_start_times_ids.count != slot_start_times_ids.uniq.count
