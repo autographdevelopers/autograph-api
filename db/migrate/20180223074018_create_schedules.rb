599208d3a83e8aa970936dfa50c9d41fbbf4e585
maciekmichalik <michalikmaciej1995@gmail.com>
2018-03-04 12:17:38 +0100

    Fix schema

--- db/migrate/20180223074018_create_schedules.rb
+++ db/migrate/20180223074018_create_schedules.rb
@@ -1,7 +1,7 @@
 class CreateSchedules < ActiveRecord::Migration[5.1]
   def change
     create_table :schedules do |t|
-      t.integer :repetition_period_in_weeks, null: false, default: 12
+      t.integer :repetition_period_in_weeks, null: false, default: 0
       t.json :current_template, null: false, default: { monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: [] }
       t.json :new_template, null: false, default: { monday: [], tuesday: [], wednesday: [], thursday: [], friday: [], saturday: [], sunday: [] }
       t.date :new_template_binding_from
