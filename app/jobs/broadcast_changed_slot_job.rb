63b274445d59e5214f35bc7d9ffd94856dd4388e
Wojciech Pospieszynski <pospieszynski.wojciech@gmail.com>
2018-03-31 23:49:46 +0200

    webosckets setup + selecting slot

--- app/jobs/broadcast_changed_slot_job.rb
+++ app/jobs/broadcast_changed_slot_job.rb
@@ -6,11 +6,13 @@ class BroadcastChangedSlotJob < ApplicationJob
 
     ActionCable.server.broadcast(
       build_channel_string(slot.employee_driving_school_id),
-      type: 'SLOT_CHANGED',
-      slot: ApplicationController.renderer.render(
-        partial: 'api/v1/slots/slot.json',
-        locals: { slot: slot }
-      )
+      {
+        type: 'SLOT_CHANGED',
+        slot: JSON.parse(ApplicationController.renderer.render(
+          partial: 'api/v1/slots/slot.json',
+          locals: { slot: slot }
+        ))
+      }
     )
   end
 end
